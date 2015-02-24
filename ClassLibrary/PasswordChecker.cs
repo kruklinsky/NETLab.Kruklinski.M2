6c39f63a091544e08f997a1b6f39195d3c1de87e
kruklinsky <kruklinsky.alexander@gmail.com>
2015-02-24 21:01:27 +0300

    Add solution

---
+++ ClassLibrary/PasswordChecker.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class PasswordChecker
+    {
+        private IUserRepository userRepository;
+
+        public PasswordChecker(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public Tuple<bool, string> Verify(string password, bool isAdmin = false)
+        {
+            Tuple<bool, string> result;
+            if (password == null)
+            {
+                throw new System.ArgumentNullException("password", "Password is null.");
+            }
+            if (isAdmin)
+            {
+                result = VerifyCases(password, IsLengthMoreThenTen, IsContainsDigit, IsContainsLetter,IsContainsSpecialСharacter);
+            }
+            else
+            {
+                result = VerifyCases(password, IsLengthMoreThenSeven, IsContainsDigit, IsContainsLetter);
+            }
+            if (result.Item1) this.userRepository.CreateUser(password);
+            return result;
+        }
+
+        #region Private methods
+
+        private Tuple<bool, string> IsContainsSpecialСharacter(string str)
+        {
+            Tuple<bool, string> result = new Tuple<bool, string>(true, "OK");
+            if (str.Where(c => IsSpecialСharacter(c)).Count() < 1)
+            {
+                result = new Tuple<bool, string>(false, "Number of special characters is less then one.");
+            }
+            return result;
+        }
+        private Tuple<bool, string> IsContainsDigit(string str)
+        {
+            Tuple<bool, string> result = new Tuple<bool, string>(true, "OK");
+            if (str.Where(c => char.IsDigit(c)).Count() < 1)
+            {
+                result = new Tuple<bool, string>(false, "Number of digits is less then one.");
+            }
+            return result;
+        }
+        private Tuple<bool, string> IsContainsLetter(string str)
+        {
+            Tuple<bool, string> result = new Tuple<bool, string>(true, "OK");
+            if (str.Where(c => char.IsLetter(c)).Count() < 1)
+            {
+                result = new Tuple<bool, string>(false, "Number of letters is less then one.");
+            }
+            return result;
+        }
+        private Tuple<bool, string> IsLengthMoreThenSeven(string str)
+        {
+            Tuple<bool, string> result = new Tuple<bool, string>(true, "OK");
+            if (str.Length < 7)
+            {
+                result = new Tuple<bool, string>(false, "Length is less then seven.");
+            }
+            return result;
+        }
+        private Tuple<bool, string> IsLengthMoreThenTen(string str)
+        {
+            Tuple<bool, string> result = new Tuple<bool, string>(true, "OK");
+            if (str.Length < 10)
+            {
+                result = new Tuple<bool, string>(false, "Length is less then ten.");
+            }
+            return result;
+        }
+        private bool IsSpecialСharacter(char c)
+        {
+            bool result = false;
+            switch (c)
+            {
+                case '!':
+                case '@':
+                case '#':
+                case '$':
+                case '%':
+                case '^':
+                case '&':
+                case '*':
+                    {
+                        result = true;
+                    }
+                    break;
+                default:
+                    {
+                        result = false;
+                    }
+                    break;
+            }
+            return result;
+        }
+
+        private Tuple<bool, string> VerifyCases(string str, params Func<string, Tuple<bool, string>>[] stringCases)
+        {
+            Tuple<bool, string> result;
+            foreach (var stringCase in stringCases)
+            {
+                result = stringCase(str);
+                if(result.Item1 == false)
+                {
+                    return result;
+                }
+            }
+            return new Tuple<bool, string>(true, "OK");
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTest/PasswordChecker.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using Moq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class _PasswordChecker
+    {
+        Mock<IUserRepository> userRepository;
+        private PasswordChecker checker;
+
+        [TestInitialize]
+        public void PasswordChecker_Initialize()
+        {
+            this.userRepository = new Mock<IUserRepository>();
+            this.checker = new PasswordChecker(userRepository.Object);
+        }
+
+        #region Exceptions
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void Verify_PasswordIsNull_Exception()
+        {
+            string password = null;
+
+            bool result = checker.Verify(password).Item1;
+        }
+
+        #endregion
+
+        #region User
+
+        [TestMethod]
+        public void Verify_PasswordLengthIsLessThanSeven_False()
+        {
+            bool expected = false;
+            string password = "1a3456";
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_PasswordLengthIsGretastThanSeven_True()
+        {
+            bool expected = true;
+            string password = "a234567";
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Verify_PasswordDoseNotContainDigit_False()
+        {
+            bool expected = false;
+            string password = "asdfghj";
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_PasswordContainsDigit_True()
+        {
+            bool expected = true;
+            string password = "asdfghj7";
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Verify_PasswordDoseNotContainAlphabetic_False()
+        {
+            bool expected = false;
+            string password = "1234567";
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_PasswordContainsAlphabetic_True()
+        {
+            bool expected = true;
+            string password = "asdfghj7";
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Verify_PasswordInvalid_CreateUser_Never()
+        {
+            bool expected = false;
+            string password = "123456";
+            Mock<IUserRepository> userRepository = new Mock<IUserRepository>();
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+            this.userRepository.Verify(t => t.CreateUser(It.IsAny<string>()), Times.Never);
+        }
+        [TestMethod]
+        public void Verify_PasswordValid_CreateUser_Once()
+        {
+            bool expected = true;
+            string password = "asdfghj7";
+            Mock<IUserRepository> userRepository = new Mock<IUserRepository>();
+
+            bool result = checker.Verify(password).Item1;
+
+            Assert.AreEqual(expected, result);
+            this.userRepository.Verify(t => t.CreateUser(It.IsAny<string>()), Times.Once);
+        }
+
+        #endregion
+
+        #region Admin
+
+        [TestMethod]
+        public void Verify_Admin_PasswordLengthIsLessThanTen_False()
+        {
+            bool expected = false;
+            bool isAdmin = true;
+            string password = "a2345678!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_Admin_PasswordLengthIsGretastThanTen_True()
+        {
+            bool expected = true;
+            bool isAdmin = true;
+            string password = "a23456789!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Verify_Admin_PasswordDoseNotContainDigit_False()
+        {
+            bool expected = false;
+            bool isAdmin = true;
+            string password = "asdfghjkl!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_Admin_PasswordContainsDigit_True()
+        {
+            bool expected = true;
+            bool isAdmin = true;
+            string password = "asdfghj789!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Verify_Admin_PasswordDoseNotContainAlphabetic_False()
+        {
+            bool expected = false;
+            bool isAdmin = true;
+            string password = "123456789!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_Admin_PasswordContainsAlphabetic_True()
+        {
+            bool expected = true;
+            bool isAdmin = true;
+            string password = "asdfghj789!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Verify_Admin_PasswordDoseNotContainsSpecialСharacter_False()
+        {
+            bool expected = false;
+            bool isAdmin = true;
+            string password = "asdfghj789";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_Admin_PasswordContainsSpecialСharacter_Ture()
+        {
+            bool expected = true;
+            bool isAdmin = true;
+            string password = "asdfghj789!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Verify_PasswordDoseNotContainsSpecialСharacter_True()
+        {
+            bool expected = true;
+            bool isAdmin = false;
+            string password = "asdfghj7";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Verify_Admin_PasswordInvalid_CreateUser_Never()
+        {
+            bool expected = false;
+            bool isAdmin = true;
+            string password = "123456";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+            this.userRepository.Verify(t => t.CreateUser(It.IsAny<string>()), Times.Never);
+        }
+        [TestMethod]
+        public void Verify_Admin_PasswordValid_SingIn_Once()
+        {
+            bool expected = true;
+            bool isAdmin = true;
+            string password = "asdfghj789!";
+
+            bool result = checker.Verify(password, isAdmin).Item1;
+
+            Assert.AreEqual(expected, result);
+            this.userRepository.Verify(t => t.CreateUser(It.IsAny<string>()), Times.Once);
+        }
+
+        #endregion
+    }
+}
